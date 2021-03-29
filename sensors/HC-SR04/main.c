44cd3c3cf9b7aa0b7a6ac49ea1bb76979e18bb33
Samir Teymurov <56115472+teymurov27@users.noreply.github.com>
2021-03-29 21:58:45 +0300

    Update main.c

--- sensors/HC-SR04/main.c
+++ sensors/HC-SR04/main.c
@@ -1,1 +1,28 @@
+const int trigP = 2;  //D4 Or GPIO-2 of nodemcu
+const int echoP = 0;  //D3 Or GPIO-0 of nodemcu
 
+long duration;
+int distance;
+
+void setup() {
+pinMode(trigP, OUTPUT);  // Sets the trigPin as an Output
+pinMode(echoP, INPUT);   // Sets the echoPin as an Input
+Serial.begin(9600);      // Open serial channel at 9600 baud rate
+}
+
+void loop() {
+
+digitalWrite(trigP, LOW);   // Makes trigPin low
+delayMicroseconds(2);       // 2 micro second delay 
+
+digitalWrite(trigP, HIGH);  // tigPin high
+delayMicroseconds(10);      // trigPin high for 10 micro seconds
+digitalWrite(trigP, LOW);   // trigPin low
+
+duration = pulseIn(echoP, HIGH);   //Read echo pin, time in microseconds
+distance= duration*0.034/2;        //Calculating actual/real distance
+
+Serial.print("Distance = ");        //Output distance on arduino serial monitor 
+Serial.println(distance);
+delay(1000);                        //Pause for 1 seconds and start measuring distance again
+}
